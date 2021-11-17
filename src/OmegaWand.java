0e79f2ea0d3f7e44726d26609f7838d851f5f227
Andrea Cavalli <andrea@cavallium.it>
2021-11-17 10:59:54 +0100

    99% accuracy improvement

--- src/OmegaWand.java
+++ src/OmegaWand.java
@@ -3,7 +3,7 @@ public class OmegaWand {
 	/** Main method **/
 	public static void main(String[] args) {
 		// Parse, stem, tokenize, load the indices, search, collect the results, send the results.
-		System.out.println("All your results are in the database.");
+		System.out.println("All your results may be in the database.");
 	}
 }
 /** The first software with also the last line documented **/
